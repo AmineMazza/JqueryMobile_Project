ab4c6c85408374d2a3049d489416c09b556d7126
Amine Mazza <102493810+AmineMazza@users.noreply.github.com>
2023-04-14 02:56:08 +0000

    Add files via upload

---
+++ lib/waypoints/links.php
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+<?php
+  $links = array(
+    'js' => 'lib/waypoints/waypoints.min.js'
+  );
+?>
+=======
+<?php
+  $links = array(
+    'js' => 'lib/waypoints/waypoints.min.js'
+  );
+?>
+>>>>>>> d7879ae3761eb61d94740a51885c359b03f3ed70
